f9490c7b55450130480a4e08da18a9ed7e91923b
Alessandro Brozzi <abrozzi@MBP-di-Stefano.homenet.telecomitalia.it>
2023-02-23 22:51:37 +0100

    added R parser to create a tab separated table

--- R/main.R
+++ R/main.R
@@ -1,4 +1,36 @@
+# Lib
+
 library(rjson)
 
-bioprj <- fromJSON(file = "bioproject.json")
-save(bioprj, file="bioproject.Rda")
+# Main
+
+res = fromJSON(file = "test/demo.json")
+
+N   = length(res$PackageSet$Package)
+
+ids    = c()
+titles = c()
+desc   = c()
+
+for ( i in 1:N) {
+  
+  cat (i, "\n")
+  
+  ids = c(ids,
+          res$PackageSet$Package[[i]]$Project$Project$ProjectID$ArchiveID$"@accession"
+  )
+  
+  titles = c(titles,
+             res$PackageSet$Package[[i]]$Project$Project$ProjectDescr$Title
+  )
+  
+  desc  = c(desc, 
+            res$PackageSet$Package[[i]]$Project$Project$ProjectDescr$Description
+  )
+}
+
+df = data.frame(id = ids, title = titles, description = desc, stringsAsFactors = FALSE)
+
+# Output
+
+write.table(df, file = "test/demo.tsv", sep="\t", quote = FALSE)
